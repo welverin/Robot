8652ad29f1203e26a32ecf3bec3e62eb50272899
welverin <9484183+welverin@users.noreply.github.com>
2020-12-22 00:51:27 +0100

    Update Move.py

--- Servo/Move.py
+++ Servo/Move.py
@@ -1,1 +1,15 @@
-
+import time
+from adafruit_servokit import ServoKit
+kit = ServoKit(channels=16)
+// go avanti
+kit.continuous_servo[1].throttle = 1
+time.sleep(1)
+// go indietro
+kit.continuous_servo[1].throttle = -1
+time.sleep(1)
+// mezza velocità
+kit.continuous_servo[1].throttle = 0.5
+time.sleep(1)
+//stop
+kit.continuous_servo[1].throttle = 0
+time.sleep(1)
